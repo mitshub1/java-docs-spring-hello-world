8786b2d3255920dec1f3a2fa40a375e07b20e41c
mitshub1 <131574186+mitshub1@users.noreply.github.com>
2023-04-23 16:21:57 +0900

    aa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/a")
 	String sayHello() {
 		return "Hello World!";
 	}
